afcb3f0e77ec2da71a343beb071483e3ed7b894c
Nikita <nikita-qwerty@yandex.ru>
2022-09-20 11:21:19 +0400

    mixed inputs are working now

--- scripts/db.py
+++ scripts/db.py
@@ -9,6 +9,8 @@ class Database:
         DB_PATH = path
         self._create_connection(DB_PATH)
         self.cur = self.con.cursor()
+        self.con.row_factory = lambda cur, row: row[0]
+
 
     def _create_connection(self, db_file):
         """create a database connection to a SQLite database"""
@@ -30,25 +32,33 @@ class Database:
     def add_ids_from_list(self, ids, next_cursor):
 
         data = [(id, next_cursor) for id in ids]
-        self.cur.executemany(
-            "insert into rtfkt_followers (id, next_cursor) VALUES(?, ?)", data
-        )
-        self.con.commit()
+        try:
+            self.cur.executemany(
+                "insert into rtfkt_followers (id, next_cursor) VALUES(?, ?)", data
+            )
+            self.con.commit()
+        except sqlite3.IntegrityError:
+            ids_exists = self.con.execute('''SELECT id from rtfkt_followers''').fetchall()
+            new_ids = set(ids).difference(ids_exists) 
+            new_data = [(id, next_cursor) for id in new_ids]
+            self.cur.executemany(
+                "insert into rtfkt_followers (id, next_cursor) VALUES(?, ?)", new_data
+            )
+            self.con.commit()
 
+# if __name__ == "__main__":
 
-if __name__ == "__main__":
+#     DB_PATH = os.path.join(PATH, "outputs/twitter.db")
+#     os.remove(DB_PATH)
+#     db = Database(DB_PATH)
+#     db._create_tables()
 
-    DB_PATH = os.path.join(PATH, "outputs/twitter.db")
-    os.remove(DB_PATH)
-    db = Database(DB_PATH)
-    db._create_tables()
+#     import json
 
-    import json
+#     with open(os.path.join(PATH, "outputs/rtfkt_ids.json"), "r") as f:
+#         test_ids1 = json.load(f)
+#     with open(os.path.join(PATH, "outputs/rtfkt_ids_2.json"), "r") as f:
+#         test_ids2 = json.load(f)
 
-    with open(os.path.join(PATH, "outputs/rtfkt_ids.json"), "r") as f:
-        test_ids1 = json.load(f)
-    with open(os.path.join(PATH, "outputs/rtfkt_ids_2.json"), "r") as f:
-        test_ids2 = json.load(f)
-
-    db.add_ids_from_list(test_ids1["ids"], test_ids1["next_cursor_str"])
-    db.add_ids_from_list(test_ids2["ids"], test_ids2["next_cursor_str"])
+#     db.add_ids_from_list(test_ids1["ids"], test_ids1["next_cursor_str"])
+#     db.add_ids_from_list(test_ids2["ids"], test_ids2["next_cursor_str"])
--- tests/test_database.py
+++ tests/test_database.py
@@ -20,7 +20,7 @@ def test_database_add_unique_ids(db):
     ) + len(test_ids2["ids"])
 
 @pytest.mark.xfail(raises=IntegrityError)
-def test_database_add_non_unique_ids(db):
+def test_database_add_all_non_unique_ids(db):
     ## arrange
     with open(os.path.join(PATH, "outputs/rtfkt_ids.json"), "r") as f:
         test_ids1 = json.load(f)
@@ -31,3 +31,21 @@ def test_database_add_non_unique_ids(db):
 
     ## assert
     db.cur.execute("select count(*) from rtfkt_followers").fetchone()[0] 
+
+def test_add_mixed_unique_and_non_unique_ids(db):
+    ## arrange
+    with open(os.path.join(PATH, "outputs/rtfkt_ids.json"), "r") as f:
+        test_ids1 = json.load(f)
+    with open(os.path.join(PATH, "outputs/rtfkt_ids_2.json"), "r") as f:
+        test_ids2 = json.load(f)
+
+    test_ids3 = test_ids1["ids"][-100:] + test_ids2["ids"]
+    assert len(test_ids3) == len(test_ids2['ids']) + 100
+    ## act
+    db.add_ids_from_list(test_ids1["ids"], test_ids1["next_cursor_str"])
+    db.add_ids_from_list(test_ids3, test_ids2["next_cursor_str"])
+
+    ## assert
+    assert db.cur.execute("select count(*) from rtfkt_followers").fetchone()[0] == len(
+        test_ids1["ids"]
+    ) + len(test_ids2["ids"])
